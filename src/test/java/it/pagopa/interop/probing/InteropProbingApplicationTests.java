31e344f5b7c76bc7bb8f1576869e0d21fb716d07
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-02-02 11:58:57 +0100

    First Commit

---
+++ src/test/java/it/pagopa/interop/probing/InteropProbingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pagopa.interop.probing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteropProbingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
