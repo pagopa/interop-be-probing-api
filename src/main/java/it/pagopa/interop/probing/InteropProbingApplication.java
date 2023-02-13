991750efaccf786ba4923ca4ed506b4a5f381d2a
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-02-13 17:02:33 +0100

    Job implementation for reading from S3 bucket

--- src/main/java/it/pagopa/interop/probing/InteropProbingApplication.java
+++ src/main/java/it/pagopa/interop/probing/InteropProbingApplication.java
@@ -1,11 +1,24 @@
+/*
+ * 
+ */
 package it.pagopa.interop.probing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * The Class InteropProbingApplication.
+ */
 @SpringBootApplication
+@EnableScheduling
 public class InteropProbingApplication {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(InteropProbingApplication.class, args);
 	}
