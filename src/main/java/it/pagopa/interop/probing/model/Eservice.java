b4d60c7bff5ff02eb11deebf1f70c247f094e442
acolucci <a.colucci@dxc.com>
2023-02-20 12:39:50 +0100

    EService default values and jdoc

--- src/main/java/it/pagopa/interop/probing/model/Eservice.java
+++ src/main/java/it/pagopa/interop/probing/model/Eservice.java
@@ -1,3 +1,21 @@
+/**************************************************************************
+*
+* Copyright 2023 (C) DXC
+*
+* Created on  : Feb 20, 2023
+* Author      : dxc technology
+* Project Name: interop-be-probing 
+* Package     : it.pagopa.interop.probing.model
+* File Name   : Eservice.java
+*
+*-----------------------------------------------------------------------------
+* Revision History (Release )
+*-----------------------------------------------------------------------------
+* VERSION     DESCRIPTION OF CHANGE
+*-----------------------------------------------------------------------------
+** --/1.0  |  Initial Create.
+**---------|------------------------------------------------------------------
+***************************************************************************/
 package it.pagopa.interop.probing.model;
 
 import java.io.Serializable;
@@ -25,6 +43,7 @@ import it.pagopa.interop.probing.util.EserviceType;
 import lombok.Getter;
 import lombok.Setter;
 
+// TODO: Auto-generated Javadoc
 /**
  * The persistent class for the eservices database table.
  *
@@ -32,57 +51,96 @@ import lombok.Setter;
 @Entity
 @Table(name="eservices", uniqueConstraints=@UniqueConstraint(columnNames={"eservice_id", "version_id"}))
 @TypeDef(name = "basePathType", typeClass = CustomStringArrayType.class)
+
+/**
+ * Gets the version id.
+ *
+ * @return the version id
+ */
 @Getter
+
+/**
+ * Sets the version id.
+ *
+ * @param versionId the new version id
+ */
 @Setter
 public class Eservice implements Serializable {
 
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
+	
+	/** The Constant DEFAULT_POLLING_START_TIME. */
+	private static final Time  DEFAULT_POLLING_START_TIME = Time.valueOf("00:00:00");
+	
+	/** The Constant DEFAULT_POLLING_END_TIME. */
+	private static final Time  DEFAULT_POLLING_END_TIME = Time.valueOf("23:59:59");
+	
+	/** The Constant DEFAULT_POLLING_FREQUENCY. */
+	private static final Integer  DEFAULT_POLLING_FREQUENCY = 5;
+	
+	/** The Constant DEFAULT_PROBING_ENABLED. */
+	private static final boolean  DEFAULT_PROBING_ENABLED = true;
 
+	/** The id. */
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "eservice_sequence")
     @SequenceGenerator(name = "eservice_sequence",sequenceName = "eservice_sequence", allocationSize = 1)
     private long id;
 
+    /** The base path. */
     @Column(name="base_path", columnDefinition = "text[]")
     @Type(type = "basePathType")
     private String[] basePath;
 
+    /** The eservice name. */
     @Column(name="eservice_name")
     private String eserviceName;
 
+    /** The eservice type. */
     @Column(name="eservice_type")
     @Enumerated(EnumType.STRING)
     private EserviceType eserviceType;
 
+    /** The eservice id. */
     @NotNull
     @Column(name="eservice_id")
     private UUID eserviceId;
 
+    /** The last request. */
     @Column(name="last_request")
     private Timestamp lastRequest;
 
+    /** The response received. */
     @Column(name="response_received")
     private Timestamp responseReceived;
 
+    /** The polling end time. */
     @Column(name="polling_end_time")
-    private Time pollingEndTime;
+    private Time pollingEndTime = DEFAULT_POLLING_END_TIME;
 
+    /** The polling frequency. */
     @Column(name="polling_frequency", columnDefinition = "integer default 5")
-    private Integer pollingFrequency;
+    private Integer pollingFrequency = DEFAULT_POLLING_FREQUENCY;
 
+    /** The polling start time. */
     @Column(name="polling_start_time")
-    private Time pollingStartTime;
+    private Time pollingStartTime = DEFAULT_POLLING_START_TIME;
 
+    /** The probing enabled. */
     @Column(name="probing_enabled")
-    private boolean probingEnabled;
+    private boolean probingEnabled = DEFAULT_PROBING_ENABLED;
 
+    /** The producer name. */
     @Column(name="producer_name")
     private String producerName;
 
+    /** The state. */
     @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "varchar default 'ACTIVE'")
-    private EServiceState state;
+    private EServiceState state = EServiceState.ACTIVE;
 
+    /** The version id. */
     @NotNull
     @Column(name="version_id")
     private UUID versionId;
