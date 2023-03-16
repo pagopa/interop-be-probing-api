92c5263a67c3296943532460e8bed9e89597b0c8
Melissa <melissadellorco1993@gmail.com>
2023-03-16 12:57:59 +0100

    Added logback configuration

--- src/main/java/it/pagopa/interop/probing/probingapi/exception/RestExceptionHandler.java
+++ src/main/java/it/pagopa/interop/probing/probingapi/exception/RestExceptionHandler.java
@@ -22,59 +22,59 @@ import java.util.List;
 @Slf4j
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+	/**
+	 * Manages the {@link EserviceNotFoundException} creating a new
+	 * {@link ResponseEntity} and sending it to the client with error code 404 and
+	 * information about the error
+	 * 
+	 * @param ex The intercepted exception
+	 * @return A new {@link ResponseEntity} with {@link Problem} body
+	 */
+	@ExceptionHandler(EserviceNotFoundException.class)
+	protected ResponseEntity<Problem> handleEserviceNotFoundException(EserviceNotFoundException ex) {
+		log.error(ExceptionUtils.getStackTrace(ex));
+		Problem problemResponse = createProblem(HttpStatus.NOT_FOUND, ErrorMessages.ELEMENT_NOT_FOUND,
+				ErrorMessages.ELEMENT_NOT_FOUND);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemResponse);
+	}
 
-    /**
-     * Manages the {@link EserviceNotFoundException} creating a new {@link ResponseEntity} and sending it to the client
-     * with error code 404 and information about the error
-     * @param ex The intercepted exception
-     * @return A new {@link ResponseEntity} with {@link Problem} body
-     * */
-    @ExceptionHandler(EserviceNotFoundException.class)
-    protected ResponseEntity<Problem> handleEserviceNotFoundException(EserviceNotFoundException ex) {
-        log.error(ExceptionUtils.getStackTrace(ex));
-        Problem problemResponse = createProblem(HttpStatus.NOT_FOUND,
-                ErrorMessages.ELEMENT_NOT_FOUND,
-                ErrorMessages.ELEMENT_NOT_FOUND);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemResponse);
-    }
+	/**
+	 * Manages the {@link HttpMessageNotReadableException} creating a new
+	 * {@link ResponseEntity} and sending it to the client with error code 400 and
+	 * information about the error
+	 * 
+	 * @param ex The intercepted exception
+	 * @return A new {@link ResponseEntity} with {@link Problem} body
+	 */
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.error(ExceptionUtils.getStackTrace(ex));
+		Problem problemResponse = createProblem(HttpStatus.BAD_REQUEST, ErrorMessages.BAD_REQUEST,
+				ErrorMessages.BAD_REQUEST);
+		return ResponseEntity.status(status).body(problemResponse);
+	}
 
-    /**
-     * Manages the {@link HttpMessageNotReadableException} creating a new {@link ResponseEntity}
-     * and sending it to the client with error code 400 and information about the error
-     * @param ex The intercepted exception
-     * @return A new {@link ResponseEntity} with {@link Problem} body
-     * */
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
-        log.error(ExceptionUtils.getStackTrace(ex));
-        Problem problemResponse = createProblem(HttpStatus.BAD_REQUEST,
-                ErrorMessages.BAD_REQUEST,
-                ErrorMessages.BAD_REQUEST);
-        return ResponseEntity.status(status).body(problemResponse);
-    }
-
-    /**
-     * Creates an instance of type {@link Problem} following the RFC 7807 standard
-     * @param responseCode The response error code
-     * @param titleMessage The response title message
-     * @param detailMessage The response detail error message
-     * @return A new instance of {@link Problem}
-     * */
-    private Problem createProblem(HttpStatus responseCode, String titleMessage, String detailMessage){
-        Problem genericError = new Problem();
-        genericError.setStatus(responseCode.value());
-        genericError.setTitle(titleMessage);
-        genericError.setDetail(detailMessage);
-        genericError.setTraceId(MDC.get(LoggingPlaceholders.TRACE_ID_PLACEHOLDER));
-        ProblemError errorDetails = new ProblemError();
-        errorDetails.setCode(responseCode.toString());
-        errorDetails.setDetail(detailMessage);
-        List<ProblemError> errorDetailsList = new ArrayList<>();
-        errorDetailsList.add(errorDetails);
-        genericError.setErrors(errorDetailsList);
-        return genericError;
-    }
+	/**
+	 * Creates an instance of type {@link Problem} following the RFC 7807 standard
+	 * 
+	 * @param responseCode  The response error code
+	 * @param titleMessage  The response title message
+	 * @param detailMessage The response detail error message
+	 * @return A new instance of {@link Problem}
+	 */
+	private Problem createProblem(HttpStatus responseCode, String titleMessage, String detailMessage) {
+		Problem genericError = new Problem();
+		genericError.setStatus(responseCode.value());
+		genericError.setTitle(titleMessage);
+		genericError.setDetail(detailMessage);
+		genericError.setTraceId(MDC.get(LoggingPlaceholders.TRACE_ID_PLACEHOLDER));
+		ProblemError errorDetails = new ProblemError();
+		errorDetails.setCode(responseCode.toString());
+		errorDetails.setDetail(detailMessage);
+		List<ProblemError> errorDetailsList = new ArrayList<>();
+		errorDetailsList.add(errorDetails);
+		genericError.setErrors(errorDetailsList);
+		return genericError;
+	}
 }
--- src/main/java/it/pagopa/interop/probing/probingapi/repository/EserviceRepository.java
+++ src/main/java/it/pagopa/interop/probing/probingapi/repository/EserviceRepository.java
@@ -1,15 +1,17 @@
 package it.pagopa.interop.probing.probingapi.repository;
 
-import it.pagopa.interop.probing.probingapi.model.Eservice;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
-import java.util.UUID;
+
+import it.pagopa.interop.probing.probingapi.model.Eservice;
 
 @Repository
 public interface EserviceRepository extends JpaRepository<Eservice, Long> {
 
-    Optional<Eservice> findByEserviceIdAndVersionId(@Param("eserviceId") UUID eserviceId,
-                                                           @Param("versionId") UUID versionId);
+	Optional<Eservice> findByEserviceIdAndVersionId(@Param("eserviceId") UUID eserviceId,
+			@Param("versionId") UUID versionId);
 }
