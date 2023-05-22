62574d977cd6139afbc4954a4d172357f56652b2
angelaantoniaceaa <107028078+angelaantoniaceaa@users.noreply.github.com>
2023-05-22 10:35:08 +0200

    Merge feature/IESP-139 (#21)
    Co-authored-by: Alessio Gallitano <25105748+galales@users.noreply.github.com>

--- src/main/java/it/pagopa/interop/probing/probingapi/client/ProducerClient.java
+++ src/main/java/it/pagopa/interop/probing/probingapi/client/ProducerClient.java
@@ -1,18 +1,17 @@
 package it.pagopa.interop.probing.probingapi.client;
 
-import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import it.pagopa.interop.probing.probingapi.dtos.SearchProducerNameResponse;
+import it.pagopa.interop.probing.probingapi.mapping.dto.impl.SearchProducerNameBEResponse;
 
 @FeignClient(name = "producerClient",
     url = "${api.operations.baseUrl}" + "${api.operations.producer.basePath}")
 public interface ProducerClient {
 
   @GetMapping("/")
-  ResponseEntity<List<SearchProducerNameResponse>> getProducers(
+  ResponseEntity<SearchProducerNameBEResponse> getProducers(
       @RequestParam(value = "limit", required = true) Integer limit,
       @RequestParam(value = "offset", required = true) Integer offset,
       @RequestParam(value = "producerName", required = false) String producerName);
