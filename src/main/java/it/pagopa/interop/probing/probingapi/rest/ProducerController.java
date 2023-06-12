9cd1db0053979b659fed8f81716bcf0fde6258cd
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-06-12 18:05:20 +0200

    Test xray ms communication

--- src/main/java/it/pagopa/interop/probing/probingapi/rest/ProducerController.java
+++ src/main/java/it/pagopa/interop/probing/probingapi/rest/ProducerController.java
@@ -1,14 +1,18 @@
 package it.pagopa.interop.probing.probingapi.rest;
 
 import java.util.List;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.spring.aop.XRayEnabled;
 import it.pagopa.interop.probing.probingapi.api.ProducersApi;
 import it.pagopa.interop.probing.probingapi.dtos.SearchProducerNameResponse;
 import it.pagopa.interop.probing.probingapi.service.ProducerService;
 
 @RestController
+@XRayEnabled
 public class ProducerController implements ProducersApi {
 
   @Autowired
@@ -17,6 +21,8 @@ public class ProducerController implements ProducersApi {
   @Override
   public ResponseEntity<List<SearchProducerNameResponse>> getEservicesProducers(Integer limit,
       Integer offset, String producerName) {
+    MDC.put("AWS-XRAY-TRACE-ID", AWSXRay.getCurrentSegment().getTraceId().toString());
+
     return ResponseEntity.ok(producerService.getEservicesProducers(limit, offset, producerName));
   }
 }
