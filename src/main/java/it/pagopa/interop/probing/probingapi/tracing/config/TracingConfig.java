666d3578d059a1b31187aba5844eb78b6d8275a4
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-06-12 18:05:20 +0200

    Test xray ms communication

--- src/main/java/it/pagopa/interop/probing/probingapi/tracing/config/TracingConfig.java
+++ src/main/java/it/pagopa/interop/probing/probingapi/tracing/config/TracingConfig.java
@@ -1,21 +1,28 @@
 package it.pagopa.interop.probing.probingapi.tracing.config;
 
 import javax.servlet.Filter;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.proxies.apache.http.HttpClientBuilder;
 
 @Configuration
+@Primary
 public class TracingConfig {
+
+  @Value("${spring.application.name}")
+  private String AWSXRAY_SEGMENT_NAME;
 
   @Bean
   public Filter tracingFilter() {
-    return new AWSXRayServletFilter("interop-probing-apigw-dev/dev");
+    return new AWSXRayServletFilter(AWSXRAY_SEGMENT_NAME);
   }
 
   @Bean
   public HttpClientBuilder xrayHttpClientBuilder() {
+
     return HttpClientBuilder.create();
   }
 }
