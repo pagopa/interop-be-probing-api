d4a6284901cc365ba86259a0ccf0ead0495a87e4
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-08-30 09:13:25 +0200

    Fix Error 500 when operations ms return EserviceNotFoundException

--- src/main/java/it/pagopa/interop/probing/probingapi/exception/EserviceNotFoundException.java
+++ src/main/java/it/pagopa/interop/probing/probingapi/exception/EserviceNotFoundException.java
@@ -2,9 +2,13 @@ package it.pagopa.interop.probing.probingapi.exception;
 
 public class EserviceNotFoundException extends Exception {
 
-	private static final long serialVersionUID = 3431456002092605147L;
+  private static final long serialVersionUID = 3431456002092605147L;
 
-	public EserviceNotFoundException(String message) {
-		super(message);
-	}
+  public EserviceNotFoundException(String message) {
+    super(message);
+  }
+
+  public EserviceNotFoundException() {
+    super();
+  }
 }
